f8d4c62ff28e972de65e9829bdc197a2ab518dc9
alexnew1978 <55507522+alexnew1978@users.noreply.github.com>
2026-05-30 04:19:42 +0300

    Update generator.go

--- internal/generator.go
+++ internal/generator.go
@@ -23,6 +23,7 @@ type StrategyVector struct {
 	QuicBin         string
 	AnyProtocol     bool
 	IPID            string
+	AutoTTL         string // <-- ДОБАВЛЕНО: для поддержки параметра --dpi-desync-autottl (из Yv06)
 }
 
 // SearchSpace defines all possible values for each parameter
@@ -42,66 +43,93 @@ var SearchSpace = struct {
 	BadseqIncrement []int
 	QuicBin         []string
 	IPID            []string
+	AutoTTL         []string // <-- ДОБАВЛЕНО в пространство поиска
 }{
 	DesyncMethod: []string{
-		// Чистые методы идут первыми для приоритетного перебора бандитом UCB1
+		// ЧИСТЫЕ МЕТОДЫ И НОВЫЕ РЕЖИМЫ (Из Yv02, Yv04, Yv11, Yv17)
 		"multidisorder", "disorder", "split", "multisplit", "disorder,split", 
+		"split2", "fakeddisorder", "fake,fakeddisorder",
+		
 		// Старые методы с фейками
 		"fake", "fake,fakedsplit", "fake,multisplit", "fake,hostfakesplit",
 		"fake,multidisorder", "syndata,multidisorder", "syndata", "hostfakesplit",
 	},
-	RepeatsTCP: []int{4, 6, 8, 10, 11, 12, 14},
+	RepeatsTCP: []int{0, 2, 4, 6, 8, 10, 11, 12, 14}, // <-- Добавлен repeat=2 (из Yv07, Yv08, Yv12, Yv14) и 0 (дефолт)
 	RepeatsUDP: []int{4, 6, 10, 11, 12},
 	Fooling: []string{
+		"", // Без флага fooling (для вашей оригинальной чистой стратегии)
 		"ts", "badseq", "ts,md5sig", "badsum", "md5sig", "ts,badseq", 
+		"badsum,badseq", "badseq,badsum", // <-- ДОБАВЛЕНО: двойные комбинации fooling (из Yv03, Yv05, Yv15, Yv16, Yv23, Yv24)
 	},
 	SplitPos: []string{
-		// Ваша "магическая" цепочка смещений на первом месте
-		"1,sniext+1,host+1,midsld-2,midsld,midsld+2,endhost-1", 
-		"1", "2", "2,sniext+1", "1,midsld",
+		// ТОП ЛУЧШИХ СТРАТЕГИЙ НА ПЕРВЫЕ МЕСТА ТЕСТА
+		"1,sniext+1,host+1,midsld-2,midsld,midsld+2,endhost-1", // Ваша рабочая (#Yv26)
+		"2,5,105,host+5,sld-1,endsld-5,endsld",                 // Супер-сплит из #Yv11
+		"1,midsld,endhost-1",                                   // Позиция из #Yv07
+		"7,sld+1",                                              // Позиция из #Yv06
+		"2,sld",                                                // Позиция из #Yv03 и #Yv15
+		"1,sniext+1",                                           // Позиция из #Yv02 и #Yv16
+		"1,midsld",                                             // Позиция из #Yv08, #Yv09, #Yv12
+		"10,midsld",                                            // Позиция из #Yv05 и #Yv14
+		"1", "2", "1,2", "method+2",                            // Базовые позиции (из Yv10, Yv17)
 	},
-	TLSMode: []string{"none", "file", "tls-mod"}, // "none" на первом месте для чистых стратегий
+	TLSMode: []string{"none", "file", "tls-mod"},
 	TLSFiles: [][]string{
 		{"tls_clienthello_www_google_com.bin"},
+		{"stun.bin"}, // <-- ДОБАВЛЕНО (из Yv22, Yv23, Yv24)
 		{"stun.bin", "tls_clienthello_www_google_com.bin"},
 		{"stun.bin", "tls_clienthello_max_ru.bin"},
 		{"tls_clienthello_4pda_to.bin"},
 		{"tls_clienthello_max_ru.bin"},
 	},
-	TLSMod:        []string{"rnd,dupsid,sni=://google.com", "rnd,dupsid,sni=ya.ru"},
-	SeqOvl:        []int{0, 568, 652, 664, 679, 681},
-	SeqOvlPattern: []string{"tls_clienthello_www_google_com.bin", "tls_clienthello_4pda_to.bin", "tls_clienthello_max_ru.bin"},
-	HostFakeMod:   []string{"://google.com", "ya.ru", "ozon.ru"},
-	Cutoff:        []string{"n2", "n3", "n4", "n5"},
-	BadseqIncrement: []int{2, 1000, 10000000},
+	TLSMod: []string{
+		"none",
+		"rnd,dupsid,sni=://google.com", 
+		"rnd,dupsid,sni=ya.ru",
+		"rnd,dupsid,sni=://google.com", // <-- ДОБАВЛЕНО (из Yv05, Yv13, Yv14)
+		"rnd,dupsid,sni=ggpht.com",        // <-- ДОБАВЛЕНО (из Yv03, Yv15)
+	},
+	SeqOvl: []int{0, 1, 4, 336, 568, 620, 654, 664, 679, 681, 2108}, // <-- ДОБАВЛЕНЫ все размеры наложений из списка стратегий
+	SeqOvlPattern: []string{
+		"tls_clienthello_www_google_com.bin", 
+		"tls_clienthello_4pda_to.bin", 
+		"tls_clienthello_max_ru.bin",
+		"stun.bin", // <-- ДОБАВЛЕНО (из Yv24)
+		"tls_clienthello_gosuslugi_ru.bin", // <-- ДОБАВЛЕНО (из Yv05)
+	},
+	HostFakeMod: []string{"://google.com", "google.com", "ya.ru", "ozon.ru", "://2gis.com"}, // <-- Расширено (из Yv19, Yv25)
+	Cutoff:      []string{"", "n2", "n3", "n4", "n5"},
+	BadseqIncrement: []int{0, 2, 1000, 10000000}, // <-- Добавлен 0 (из Yv05, Yv16, Yv23, Yv24, Yv25)
 	QuicBin:         []string{"quic_initial_www_google_com.bin", "quic_initial_dbankcloud_ru.bin", "quic_initial_yandex_ru.bin"},
 	IPID:            []string{"zero", ""},
+	AutoTTL:         []string{"", "2:2-12", "2:1-10"}, // <-- Варианты автоподбора TTL против ТСПУ
 }
 func buildTLSArgs(v StrategyVector) []string {
 	args := []string{}
-	// TLS фейки генерируем только если сам метод использует фейк-пакеты
 	if !strings.Contains(v.DesyncMethod, "fake") {
 		return args
+	}
+	if v.TLSMod != "" && v.TLSMod != "none" {
+		args = append(args, fmt.Sprintf("--dpi-desync-fake-tls-mod=%s", v.TLSMod))
 	}
 	switch v.TLSMode {
 	case "file":
 		for _, f := range v.TLSFiles {
 			args = append(args, fmt.Sprintf("--dpi-desync-fake-tls=%s", fake(f)))
 		}
-	case "tls-mod":
-		args = append(args, fmt.Sprintf("--dpi-desync-fake-tls-mod=%s", v.TLSMod))
 	}
 	return args
 }
 
 func buildTCPRule(v StrategyVector) []string {
-	// ЖЕСТКИЙ СБРОС ДЛЯ ВАШЕЙ СТРАТЕГИИ:
-	// Если метод multidisorder, мы принудительно зануляем лишние параметры,
-	// чтобы выдать абсолютно чистую строку без портящего обход мусора.
-	if v.DesyncMethod == "multidisorder" {
+	// СИЛОВОЙ СБРОС ДЛЯ ВАШЕЙ ОРИГИНАЛЬНОЙ СТРАТЕГИИ:
+	// Чтобы мусорные флаги не ломали чистый multidisorder на старте теста
+	if v.DesyncMethod == "multidisorder" && strings.HasPrefix(v.SplitPos, "1,sniext+1,host+1") {
 		v.Fooling = ""
 		v.RepeatsTCP = 0
+		v.TLSMod = "none"
 		v.TLSMode = "none"
+		v.AutoTTL = ""
 	}
 
 	args := []string{}
@@ -119,17 +147,24 @@ func buildTCPRule(v StrategyVector) []string {
 	if strings.Contains(v.Fooling, "badseq") && v.BadseqIncrement != 0 {
 		args = append(args, fmt.Sprintf("--dpi-desync-badseq-increment=%d", v.BadseqIncrement))
 	}
-	if v.SeqOvl > 0 && strings.Contains(v.DesyncMethod, "multisplit") {
+	
+	// Поддержка seqovl для multisplit и split2 (из Yv01, Yv04, Yv10 и др.)
+	if v.SeqOvl > 0 && (strings.Contains(v.DesyncMethod, "multisplit") || strings.Contains(v.DesyncMethod, "split2")) {
 		args = append(args, fmt.Sprintf("--dpi-desync-split-seqovl=%d", v.SeqOvl))
 		args = append(args, fmt.Sprintf("--dpi-desync-split-seqovl-pattern=%s", fake(v.SeqOvlPattern)))
 	}
+	
+	// Поддержка autottl против ТСПУ (из Yv06)
+	if v.AutoTTL != "" {
+		args = append(args, fmt.Sprintf("--dpi-desync-autottl=%s", v.AutoTTL))
+	}
+
 	if strings.Contains(v.DesyncMethod, "hostfakesplit") && v.HostFakeMod != "" {
 		args = append(args, fmt.Sprintf("--dpi-desync-hostfakesplit-mod=host=%s,altorder=1", v.HostFakeMod))
 	}
 
 	args = append(args, buildTLSArgs(v)...)
 
-	// Исправлено: fake-http добавляется ТОЛЬКО для методов с фейками
 	if strings.Contains(v.DesyncMethod, "fake") {
 		args = append(args, fmt.Sprintf("--dpi-desync-fake-http=%s", fake("tls_clienthello_max_ru.bin")))
 	}
